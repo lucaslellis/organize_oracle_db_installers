12fa29bb4929877c60ebfb9bcf0ec4ff424da9a1
Lucas Pimentel Lellis <lucaslellis@gmail.com>
2022-06-16 09:37:05 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # organize_oracle_db_installers
-Organizes oracle database installers downloaded from edelivery.oracle.com
+
+Organizes oracle database installers downloaded from [Oracle Software Delivery Cloud](https://edelivery.oracle.com/).
+
+## Instructions
+
+Go [Oracle Software Delivery Cloud](https://edelivery.oracle.com/), create a cart containing the installers you want
+and download the `wget.sh` script.
+
+Copy `wget.sh` contents to the script [download_all.sh](download_all.sh) replacing the placeholder `<<ADD wget.sh contents HERE>>`.
+
+## Supported Versions
+
+* 12.1+ for Linux x64 and Windows x64
---
+++ download_all.sh
@@ -0,0 +1,38 @@
+<<ADD wget.sh contents HERE>>
+
+mkdir -p {12.1.0.1,12.1.0.2,12.2.0.1,18c,19c,21c}/{installers,patches}/{linux_x64,windows_x64}
+
+mv V1011496-01.zip 21c/installers/linux_x64
+mv V1011504-01.zip 21c/installers/linux_x64
+mv V1017685-01.zip 21c/installers/windows_x64
+mv V1017692-01.zip 21c/installers/windows_x64
+
+mv V978967-01.zip 18c/installers/linux_x64
+mv V978971-01.zip 18c/installers/linux_x64
+mv V979518-01.zip 18c/installers/windows_x64
+mv V979522-01.zip 18c/installers/windows_x64
+
+mv V46095-01_1of2.zip 12.1.0.2/installers/linux_x64
+mv V46095-01_2of2.zip 12.1.0.2/installers/linux_x64
+mv V46096-01_1of2.zip 12.1.0.2/installers/linux_x64
+mv V46096-01_2of2.zip 12.1.0.2/installers/linux_x64
+mv V47115-01_1of2.zip 12.1.0.2/installers/windows_x64
+mv V47115-01_2of2.zip 12.1.0.2/installers/windows_x64
+mv V46096-01_1of2.zip 12.1.0.2/installers/windows_x64
+mv V46096-01_2of2.zip 12.1.0.2/installers/windows_x64
+
+mv V38500-01_1of2.zip 12.1.0.1/installers/linux_x64
+mv V38500-01_2of2.zip 12.1.0.1/installers/linux_x64
+mv V38501-01_1of2.zip 12.1.0.1/installers/linux_x64
+mv V38501-01_2of2.zip 12.1.0.1/installers/linux_x64
+mv V38894-01_1of2.zip 12.1.0.1/installers/windows_x64
+mv V38894-01_2of2.zip 12.1.0.1/installers/windows_x64
+mv V38896-01_1of2.zip 12.1.0.1/installers/windows_x64
+mv V38896-01_2of2.zip 12.1.0.1/installers/windows_x64
+mv V38321-01.zip 12.1.0.1/installers/windows_x64
+mv V38321-01.zip 12.1.0.1/installers/windows_x64
+
+mv V982063-01.zip 18c/installers/linux_x64
+mv V982068-01.zip 18c/installers/linux_x64
+mv V982656-01.zip 18c/installers/windows_x64
+mv V982662-01.zip 18c/installers/windows_x64
